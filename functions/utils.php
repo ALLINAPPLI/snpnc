2c35605fea5ab631634ad9e0973a645a93b81495
dewy <dewymercerais@gmail.com>
2023-11-17 18:34:23 +0100

    Modification du hook pour la soumission du formulaire avec l'extension contact form 7 + ajout du mapping pour les champs ACF : select, text, radio

--- functions/utils.php
+++ functions/utils.php
@@ -1,1 +1,20 @@
 <?php
+
+    function mappingAcfFields($submitFormData, $userData) {
+        
+        $userId = $userData->data->ID;
+        // mapping telephone : text field
+        update_field('user_telephone',trim($submitFormData->get_posted_data('telephone')), 'user_' . $userId);
+    
+        // mapping fonction : select field
+        $fonction = $submitFormData->get_posted_data('fonction');
+        update_field('fonction',trim($fonction[0]), 'user_' . $userId);
+        
+        // mapping type de contrat : radio field
+        $typedecontrat = $submitFormData->get_posted_data('typedecontrat');
+        update_field('type-de-contrat',trim($typedecontrat[0]), 'user_' . $userId);
+        
+        // mapping compagnie : select compagnie
+        $compagnie = $submitFormData->get_posted_data('compagnie');
+        update_field('la_compagnie', trim($compagnie[0]), 'user_' . $userId);
+    }
